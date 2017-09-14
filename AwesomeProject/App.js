65398899cd42fe9cbafa3e320c2b4429daf4bbd0
Chen Yan <cyan@wakefly.com>
2017-09-14 13:33:02 -0400

    first commit

---
+++ AwesomeProject/App.js
@@ -0,0 +1,47 @@
+import React, {Component} from 'react';
+import { StyleSheet, Text, View, Image } from 'react-native';
+
+export default class App extends Component {
+  render() {
+    return (
+      <View style={styles.container}>
+        <Text>Open up App.js to start working on your app!</Text>
+        <Text>Changes you make will automatically reload.</Text>
+        <Text>Shake your phone to open the developer menu.</Text>
+        <Text>Testing live reload changes.</Text>
+        <Bananas />
+        <Greeting name="Rexxar" />
+        <Greeting name="Jaina" />
+        <Greeting name="Valeera" />
+      </View>
+    );
+  }
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#eee',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+});
+
+class Bananas extends Component {
+  render() {
+    let pic = {
+      uri: 'https://upload.wikimedia.org/wikipedia/commons/d/de/Bananavarieties.jpg'
+    };
+    return (
+      <Image source={pic} style={{width:193, height:110}} />
+    )
+  }
+}
+
+class Greeting extends Component {
+  render() {
+    return (
+      <Text>Hello {this.props.name}</Text>
+    )
+  }
+}
